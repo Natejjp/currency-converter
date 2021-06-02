5f60ee0d41ffa13df2b45df469130ff3799e8fc3
Nathan Phame <diyf2021@gmail.com>
2021-06-02 14:41:25 -0400

    Added axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export function App() {
+  const [currency, setCurrency] = useState({})
+
+  useEffect(async function () {
+    const response = await axios.get(
+      'http://api.exchangeratesapi.io/v1/latest?access_key=56a4348392b313cd26250dccbcbb645f'
+    )
+    if (response.status === 200) {
+      console.log(response.base)
+      setCurrency(response.rate.USD)
+    }
+  }, [])
+
   return (
     <main>
       <h1>Currency Converter</h1>
