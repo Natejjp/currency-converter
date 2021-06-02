d1adcc4a9c370b98c3f915d16d237777879de268
Nathan Phame <diyf2021@gmail.com>
2021-06-02 13:57:23 -0400

    Static html and css layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <main>
+      <h1>Currency Converter</h1>
+      <ul>
+        <li>Enter Amount USD </li>
+        <input type="text" />
+
+        <button>Convert</button>
+        <li>AUD value</li>
+        <li>CAD value</li>
+        <li>CHF value</li>
+        <li>CNY value</li>
+        <li>JPY value</li>
+        <li>USD value</li>
+      </ul>
+    </main>
+  )
 }
