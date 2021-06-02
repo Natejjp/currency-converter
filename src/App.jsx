4aa7b206dfd2d24e6223c58233e3a3273d3a1511
Nathan Phame <diyf2021@gmail.com>
2021-06-02 17:39:05 -0400

    Able to display name of currency and value based off of EUR value of 1

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,26 @@ export function App() {
       'http://api.exchangeratesapi.io/v1/latest?access_key=1b2897f6810c52c1b0c6f9a085306523'
     )
 
-    console.log(response.data)
+    setCurrency(response.data)
   }, [])
 
   return (
     <main>
       <h1>Currency Converter</h1>
       <ul>
+        {Object.entries(currency.rates).map(
+          ([currencyCode, currencyDetails]) => {
+            return (
+              <li key={currencyCode}>
+                {currencyCode}: {currencyDetails}
+              </li>
+            )
+          }
+        )}
         <li>Enter Amount USD </li>
         <input type="text" />
 
         <button>Convert</button>
-        <li>AUD value</li>
-        <li>CAD value</li>
-        <li>CHF value</li>
-        <li>CNY value</li>
-        <li>JPY value</li>
-        <li>USD value</li>
       </ul>
     </main>
   )
